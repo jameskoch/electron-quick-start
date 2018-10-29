69ee8609ab90e00536da4a06c1a24d54cb71e15b
James Koch <dev.Koch2.James@dev.bwater.com>
2018-10-29 10:45:19 -0400

    Demonstrate a way to crash Electron when run on Windows

--- main.js
+++ main.js
@@ -48,3 +48,8 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+////// PROBLEMATIC ////////
+app.on('before-quit', function(e) {
+  BrowserWindow.getAllWindows().forEach(function(win) { win.close(); });
+})
